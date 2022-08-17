8e5bac30e96101c3f9d1d818e2e906151b448a5f
olaism <olaism01@gmail.com>
2022-08-16 17:54:28 -0700

    Added instructions into run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=olaism/microservice:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy microservice --image=olaism/microservice:latest
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/microservice-84857d9565-2598m --address 0.0.0.0 8000:80
